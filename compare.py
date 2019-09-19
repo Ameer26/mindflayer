ca69579e06ccfd00e69b03fd336694307e2a80b5
mohamedameersuhail37@gmail.com <mohamedameersuhail37@gmail.com>
2019-09-19 10:08:58 +0530

    sic mundus creatus est

--- compare.py
+++ compare.py
@@ -1,9 +1,9 @@
-if(int(input("A:"))>int(input("B:"))):
-     print("A is larger B is smaller")
+a=int(input("A:"))
+b=int(input("B:"))
+c=int(input("C:"))
+if((a>=b)and(a>=c)):
+     print("A is larger than B and C")
+elif((b>=a)and(b>=c)):
+    print("B is larger than A and C")
 else:
-    print("B is larger A is smaller")
-if(int(input("A:"))>int(input("B:"))):
-     print("A is larger B is smaller")
-else:
-    print("B is larger A is smaller")
-
+     print("C is larger than A and B")
